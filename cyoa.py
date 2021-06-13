647eea855d372cf2f1e2df393bb9315db27c56e0
Edvard Ekström <edvard.ekstrom@gmail.com>
2021-06-13 08:32:27 +0000

    spelbar

--- cyoa.py
+++ cyoa.py
@@ -4,37 +4,42 @@ import random
 import asyncio
 
 async def main(channel, debuginit):
-
 	debug = False
-	# init cyoa
-	# printa prompt från adventures
-	#if not adventure:
-	#	adventure = 1
 	
 	await playAdventure(channel, debug)
 
 async def playAdventure(channel, debug):
 	page = 1
-	
-	vinst = 0
+	await Prompt(channel, page, debug)
 
-	if vinst == 0:
-		await Prompt(channel, page, debug)
-
+async def endAdventure(channel, debug):
+	end_message = \
+	"```" \
+	+ "Slutet gott, allting gott...?" \
+	+ "```" 
+	await channel.send(end_message)
 
 async def Prompt(channel, page, debug):
-	prompt_message = str(renderPromptMessage(page))
+	End = getEnd(page)
+	
+	prompt_message = str(renderPromptMessage(page, End))
 
 	refers = getRefer(page)
 	first_refer = refers[0]
 	second_refer = refers[1]
 
 	sent_message = await channel.send(prompt_message)
-	await sent_message.add_reaction("1️⃣")
-	await sent_message.add_reaction("2️⃣")
-	await asyncio.sleep(20)
-	voteresult = await Vote(channel, sent_message.id, debug, first_refer, second_refer)
-	await Prompt(channel, voteresult, debug)
+
+	if End:
+		await endAdventure(channel, debug)
+
+	else:
+		await sent_message.add_reaction("1️⃣")
+		await sent_message.add_reaction("2️⃣")
+		await asyncio.sleep(20)
+		voteresult = await Vote(channel, sent_message.id, debug, first_refer, second_refer)
+		# todo skicka ett litet svar efter val
+		await Prompt(channel, voteresult, debug)
 
 async def Vote(channel, message_id, debug, first_refer, second_refer):
 	
@@ -60,34 +65,38 @@ async def Vote(channel, message_id, debug, first_refer, second_refer):
 	else:
 		return random.randint(1,2)
 
-def renderPromptMessage(page):
+def renderPromptMessage(page, end):
+	#ska fixa bättre data system :)
+	# todo stöd för fler val !
 	adventure_content = adventure[str(page)]
 	prompt_t = adventure_content[0]
 	first_t = adventure_content[1]
 	second_t = adventure_content[3]
 	
-	#if adventure_content[6]:
-	#	third_t = adventure_content[6]
-	#if adventure_content[7]:
-	#	third_r = adventure_content[7]
-	#if adventure_content[8]:
-	#	vinstsida = adventure_content[8]
 
 	prompt_message = \
 		"```" \
 		+ prompt_t \
-		+ "```" \
 		+ "\n" \
-		+ "```" \
+		+ "\n" \
+		+ "\n" \
+		+ "--- \n" \
 		+ "1️⃣: " \
 		+ first_t \
-		+ "```" \
 		+ "\n" \
-		+ "```" \
 		+ "2️⃣: " \
 		+ second_t \
 		+ "```" 
-	return prompt_message
+
+	end_message = \
+		"```" \
+		+ prompt_t \
+		+ "```" \
+	
+	if end:
+		return end_message
+	else:
+		return prompt_message
 
 def getRefer(page):
 	refer_content = adventure[str(page)]
@@ -97,29 +106,44 @@ def getRefer(page):
 
 	return first_refer, second_refer
 
+def getEnd(page):
+	end_content = adventure[str(page)]
+
+	End = end_content[5]
+
+	return End
 
 
 
-
-
+# det är detta som ska bli bättre asså
 adventure = {
 	"1": [
-		"Du möter en ensam vandrare. Vad gör du?", 
-		"Jag går till havet.", 2, 
-		"Jag går till skogs.", 3],
+		"Mitt namn är Teo och jag jobbar i dildofabriken. \n Klockan ringer! Dags att vakna och åka till jobbet!", 
+		"Jag cyklar till jobbet på min enhjuling.", 2, 
+		"Jag flyger till jobbet i mitt lilla propellerplan.", 2,
+		False],
 
 	"2": [
-		"Du är vid havet. Nu då?", 
-		"Gå tillbaks till vandraren.", 1, 
-		"Gå till skogs.", 3],
+		"Äntligen på jobbet! Dags att suga dildos! \n På vägen till dildosugarrummet går jag förbi en kollega.", 
+		"Hej Stefan!", 3, 
+		"Ingen tid för snack - det finns dildos att suga!", 4,
+		False],
 
 	"3": [
-		"Du är i skogen. Du kommer att dö här.", 
-		"Gå o skit på dej!", 4, 
-		"Gå o dö i havet!", 2],
+		"Hej Teo! svarar min kollega Stefan Hebis.", 
+		"Hej Stefan!", 3, 
+		"Hejdå Stefan!", 4, 
+		False],
 
 	"4": [
-		"Du är på väg någonstans illa.", 
-		"Skiter jag i.", 4, 
-		"Sug min jerry!", 1],
+		"Äntligen framme i dildosugarrummet. Nu är det dags för mitt jobb att börja!", 
+		"Mums!", 5, 
+		"Smask!", 5,
+		False],
+	"5": [
+		"Ja älsk å sug dildos ja!",
+		"",5,
+		"",5,
+		True],
+	
 }
